672e60357bc131afad1e8f8231dd1db119aed4ef
oddtO <121516833+oddtO@users.noreply.github.com>
2024-06-29 02:04:23 +0300

    predict on loading screen the size of the assets to download

--- src/components/loading-screen/component.tsx
+++ src/components/loading-screen/component.tsx
@@ -1,6 +1,8 @@
 import styles from "./styles.module.scss";
 import type { Preloader } from "../../use-preload";
 import ProgressBar from "../progress-bar/component";
+
+const PREDICTED_TOTAL_ASSET_SIZE = 1600000;
 export default function LoadingScreen({
   preloaders,
 }: {
@@ -14,8 +16,6 @@ export default function LoadingScreen({
   );
   if (isAnyError) {
     detailedText = "Network error occured while loading";
-  } else if (!isTotalFullBytesKnown) {
-    detailedText = "Calculating content size...";
   } else {
     detailedText = "Loading assets...";
   }
@@ -34,11 +34,13 @@ export default function LoadingScreen({
     <div className={styles.loadingScreen}>
       <div
         className={styles.textInfo}
-      >{`${convertToKb(totalBytesLoaded)} / ${isTotalFullBytesKnown ? convertToKb(totalBytesToLoad) : "???"}`}</div>
+      >{`${convertToKb(totalBytesLoaded)} / ${isTotalFullBytesKnown ? convertToKb(totalBytesToLoad) : convertToKb(PREDICTED_TOTAL_ASSET_SIZE)}`}</div>
       <ProgressBar
         className={styles.progressBar}
         value={totalBytesLoaded}
-        max={isTotalFullBytesKnown ? totalBytesToLoad : Infinity}
+        max={
+          isTotalFullBytesKnown ? totalBytesToLoad : PREDICTED_TOTAL_ASSET_SIZE
+        }
       ></ProgressBar>
       <div className={styles.textInfo + (isAnyError ? " " + styles.error : "")}>
         {detailedText}
